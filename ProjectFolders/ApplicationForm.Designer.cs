8ab4418d0395d89555f1eaf2fd3f437af5aa2a93
HurrayBanana <hurraybanana@gmail.com>
2017-05-05 12:16:56 +0100

    Fixed interface and form structure

---
+++ ProjectFolders/ApplicationForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ProjectFolders
+{
+    partial class ApplicationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bOperate = new System.Windows.Forms.Button();
+            this.bCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // bOperate
+            // 
+            this.bOperate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bOperate.Location = new System.Drawing.Point(97, 4);
+            this.bOperate.Name = "bOperate";
+            this.bOperate.Size = new System.Drawing.Size(286, 31);
+            this.bOperate.TabIndex = 0;
+            this.bOperate.Text = "Select Folder File";
+            this.bOperate.UseVisualStyleBackColor = true;
+            this.bOperate.Click += new System.EventHandler(this.bOperate_Click);
+            // 
+            // bCancel
+            // 
+            this.bCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCancel.Location = new System.Drawing.Point(630, 4);
+            this.bCancel.Name = "bCancel";
+            this.bCancel.Size = new System.Drawing.Size(100, 31);
+            this.bCancel.TabIndex = 1;
+            this.bCancel.Text = "Cancel";
+            this.bCancel.UseVisualStyleBackColor = true;
+            this.bCancel.Click += new System.EventHandler(this.bCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Output";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(1, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(729, 220);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // ApplicationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 262);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bCancel);
+            this.Controls.Add(this.bOperate);
+            this.Name = "ApplicationForm";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.ApplicationForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bOperate;
+        private System.Windows.Forms.Button bCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ ProjectFolders/ApplicationForm.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectFolders
+{
+    public partial class ApplicationForm : Form
+    {
+        State nextAction;
+        private OpenFileDialog oFilePicker;
+        private FolderBrowserDialog fDestination;
+
+        public ApplicationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void bOperate_Click(object sender, EventArgs e)
+        {
+            switch (nextAction)
+            {
+                case State.loadFolderFile:
+                    GetFolderFile();
+                    break;
+                case State.selectingDestination:
+                    GetDestination();
+                    break;
+                case State.writingFolders:
+                    Generate(oFilePicker.FileName, fDestination.SelectedPath);
+                    break;
+            }
+
+
+        }
+
+        private void GetDestination()
+        {
+            fDestination = new FolderBrowserDialog();
+            Text = "Project Folder Generator " + "-> Select location to create folder structure";
+
+            if (fDestination.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.AppendText("\nWriting to folder:" + fDestination.SelectedPath);
+                ShowNewEntries();
+                nextAction = State.writingFolders;
+                bOperate.Text = "Write Folder Structure";
+                bCancel.Show();
+            }
+            
+        }
+
+        private void ShowNewEntries()
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            // scroll it automatically
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void GetFolderFile()
+        {
+            richTextBox1.Text = "";
+            oFilePicker = new OpenFileDialog();
+            Text = "Project Folder Generator " + "-> Select file containing folder names";
+            if (oFilePicker.ShowDialog() == DialogResult.OK)
+            {
+                OutputFile(oFilePicker.FileName);
+                nextAction = State.selectingDestination;
+                bOperate.Text = "Select Destination Folder";
+                bCancel.Show();
+            }
+        }
+        private void OutputFile(string fileName)
+        {
+            richTextBox1.AppendText("\nReading from:" + fileName);
+            richTextBox1.AppendText(" contains....\n");
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    richTextBox1.AppendText(sr.ReadLine() + "\n");
+                }
+            }
+            richTextBox1.AppendText("...finished reading file (scroll up to check)\n");
+
+            ShowNewEntries();
+
+        }
+
+        private void Generate(string fileName, string selectedPath)
+        {
+            bCancel.Hide();
+            richTextBox1.AppendText("\nWriting folders from:" + fileName);
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string foldername;
+                while (!sr.EndOfStream)
+                {
+                    foldername = Path.Combine(selectedPath, sr.ReadLine());
+                    //foldername = selectedPath + "\\" + sr.ReadLine();
+                    //Directory.CreateDirectory(foldername);
+                    richTextBox1.AppendText("\n" + foldername + " <-- created");
+                }
+                richTextBox1.AppendText("\n...Writing finished");
+
+            }
+            nextAction = State.loadFolderFile;
+            bOperate.Text = "Select Folder File";
+            ShowNewEntries();
+
+        }
+
+        private void ApplicationForm_Load(object sender, EventArgs e)
+        {
+            nextAction = State.loadFolderFile;
+            bCancel.Hide();
+        }
+
+        private void bCancel_Click(object sender, EventArgs e)
+        {
+            nextAction = State.loadFolderFile;
+            bOperate.Text = "Select Folder File";
+            richTextBox1.AppendText("\n...process aborted");
+
+            bCancel.Hide();
+        }
+    }
+}
