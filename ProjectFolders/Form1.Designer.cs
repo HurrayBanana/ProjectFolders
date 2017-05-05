0239f68da36f1bb52355e73b9e1af0a4a3fc655d
HurrayBanana <hurraybanana@gmail.com>
2017-05-05 11:29:46 +0100

    first comitt

---
+++ ProjectFolders/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ProjectFolders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bFileSelect = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bFileSelect
+            // 
+            this.bFileSelect.Location = new System.Drawing.Point(12, 12);
+            this.bFileSelect.Name = "bFileSelect";
+            this.bFileSelect.Size = new System.Drawing.Size(124, 55);
+            this.bFileSelect.TabIndex = 0;
+            this.bFileSelect.Text = "Start Process";
+            this.bFileSelect.UseVisualStyleBackColor = true;
+            this.bFileSelect.Click += new System.EventHandler(this.bFileSelect_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(156, 31);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(665, 115);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Output";
+            // 
+            // bCancel
+            // 
+            this.bCancel.Location = new System.Drawing.Point(12, 73);
+            this.bCancel.Name = "bCancel";
+            this.bCancel.Size = new System.Drawing.Size(124, 55);
+            this.bCancel.TabIndex = 3;
+            this.bCancel.Text = "Cancel Process";
+            this.bCancel.UseVisualStyleBackColor = true;
+            this.bCancel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 151);
+            this.Controls.Add(this.bCancel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.bFileSelect);
+            this.Name = "Form1";
+            this.Text = "Project Folder Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bFileSelect;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bCancel;
+    }
+}
+
